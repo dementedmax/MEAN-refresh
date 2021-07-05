8c8e2337d7ae8892b997eb609a6a9ac954f7be38
Max <chedov2000@gmail.com>
2021-07-05 15:57:29 +0600

    DB and static folder
    - Add config file with db connection cofigs
    - Export db connection configs to 'index.js'
    - Add db connection handler
    - Add static folder 'public'

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const Cors = require("cors");
 const bodyParser = require("body-parser");
-const Mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const passport = require("passport");
 const path = require("path");
 
-const app = express(); // Initiate app
-const port = 3000;     // Set port
+const config = require('./config/db');                      // Export db config file
 
-app.get('/', (req, res) => { // Simple response if '/' address will be open
+const app = express();                                      // Initiate app
+const port = 3000;                                          // Set port
+
+mongoose.connect(config.db);                                // Try connect to db
+mongoose.connection.on('connected', () =>{
+    console.log("Connect to db: successful");               // Print a message if connection successed
+});
+mongoose.connection.on('error', (err) =>{
+    console.log("Connect to db: failed");                   // Print a message if connection failed
+    console.log("Error: " + err);
+});
+
+app.use(
+    express.static(path.join(__dirname, 'public'))          // Add to 'app' static folder 'public'
+);
+
+app.get('/', (req, res) => {                                // Simple response if '/' address will be open
     res.send('Hello!');
 });
 
-app.listen(port, () => {                                 // Start listen port
-    console.log("Server will be serve on port: " + port) // Type init massege
+app.listen(port, () => {                                    // Start listen port
+    console.log("Server will be serve on port: " + port)    // Print a massege
 });
