16a3a364fdd3ffb12fe0d8b7d841f9301de7cbf4
Max <chedov2000@gmail.com>
2021-07-08 17:17:23 +0600

    Reg component init

--- front-end-app/src/app/app.module.ts
+++ front-end-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RegComponent } from './reg/reg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegComponent
   ],
   imports: [
     BrowserModule,
